65a9c08f0fe6a25ddabbd03116797ec557e254a3
Tarun Dhillon <tarundhillon14330@gmail.com>
2024-10-22 15:13:37 +0530

    3.15

--- index.js
+++ index.js
@@ -15,28 +15,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 
 
 const getId = () => {
@@ -54,21 +33,35 @@ app.get('/api/persons', (request,response)=>{
     })
 })
 
-app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</br>${new Date()}</p>`);
-});
+// app.get('/info', (request, response) => {
+//     response.send(`<p>Phonebook has info for ${persons.length} people</br>${new Date()}</p>`);
+// });
 
-app.get('/api/persons/:id', (request,response)=>{
-    Person.findById(request.params.id).then(result =>{
-        response.json(result)
+app.get('/api/persons/:id', (request,response,next)=>{
+    Person.findById(request.params.id)
+    .then(result =>{
+        if(result){
+            response.json(result)
+        }
+        else{
+            response.status(404).end()
+        }
+    })
+    .catch(error =>{
+            console.log(error)
+            response.status(400).send({error : 'Malformatted id'})
     })
 })
 
 
 app.delete('/api/persons/:id',(request,response)=>{
-    const id = request.params.id
-    persons = persons.filter(p => p.id!==id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+        console.log(error)
+    })
 })
 
 app.post('/api/persons', (request,response)=>{
